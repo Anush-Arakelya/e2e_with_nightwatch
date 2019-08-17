026ac1237dd452b4c8751456367301b408c6168e
Anush <anusharakelyan90@gmail.com>
2019-08-17 20:59:30 +0200

    Add test for Google Advanced Search

--- tests/googleSearch.js
+++ tests/googleSearch.js
@@ -4,19 +4,26 @@ module.exports = {
         const mainQuary = 'Anush Ar'
         const mainQuaryInputSelector = 'input[name = "as_q"]'
         const languageDropdownOpenSelector = '#lr_button'
+        const languageDropdownSelectValue = 'li[value = "lang_hy"]'
+        const lastUpdateDropdownSelector = '#as_qdr_button'
+        const lastUpdateDropdownSelectedValue = 'li[value = "d"]'
+        const submitButtonSelector = '.dUBGpe[type = "submit"]'
 
         browser
             .resizeWindow(1280, 1024)
             .url('https://www.google.com/advanced_search')
             .setValue(mainQuaryInputSelector, mainQuary)
-            .pause(1000)
-
-            // .click('span.recaptcha-checkbox.goog-inline-block.recaptcha-checkbox-unchecked.rc-anchor-checkbox') 
-            // .waitForElementVisible('.search_period')
-            .pause(1000)
-
+            .click(languageDropdownOpenSelector) 
+            .click(languageDropdownSelectValue) 
+            .click(lastUpdateDropdownSelector) 
+            .click(lastUpdateDropdownSelectedValue) 
+            .click(submitButtonSelector) 
+            .assert.urlContains('as_q=Anush+Ar', 'qury is Anush Ar')
+            .assert.urlContains('lang_hy', 'langauage is armenian')
+            .assert.urlContains('as_qdr=d', 'time period is last day')
             .saveScreenshot('tests_output/google.png')
-
             browser.end()
     }
+
+    
 };
